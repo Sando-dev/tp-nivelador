ead449df43eaa5adaac3870e139dd136c9d6dec2
Bauti Corti <bautistamariacorti@gmail.com>
2026-08-31 00:06:47 +0000

    socket communication handles short read/write

--- services/client/src/safe_socket/safe_socket.go
+++ services/client/src/safe_socket/safe_socket.go
@@ -4,19 +4,35 @@ import "io"
 
 //TODO: Complete with a short-read/short-write tolerant implementation
 
-func SendAll(socket io.Writer, bytes []byte) error {
-	_, err := socket.Write(bytes)
-	if err != nil {
-		return err
+func ShortWrite(socket io.Writer, bytes []byte) (int, error) {
+	sent := 0
+	for sent < len(bytes) {
+		n, err = socket.Write(bytes[sent:])
+		if err != nil {
+			return sent, err
+		}
+		sent += n
+	}	
+	return sent, nil
+}
+
+func ShortRead(socket io.Reader, size int) ([]byte, error) {
+	buff := make([]byte, size)
+	received := 0
+	for received < size {
+		n, err := socket.Read(buff[received:size])
+		if err != nil {
+			return nil, err
+		}
+		received += n
 	}
-	return nil
+	return buff, nil
+}
+
+func SendAll(socket io.Writer, bytes []byte) error {
+	return ShortWrite(socket, bytes)
 }
 
 func RecvAll(socket io.Reader, size int) ([]byte, error) {
-	buff := make([]byte, size)
-	n, err := socket.Read(buff)
-	if err != nil {
-		return nil, err
-	}
-	return buff[:n], nil
+	return ShortRead(socket, size)
 }
--- services/server/src/safe_socket/safe_socket.py
+++ services/server/src/safe_socket/safe_socket.py
@@ -3,7 +3,9 @@ import socket
 # TODO: Complete with a short-read/short-write tolerant implementation
 
 
-def recv_all(socket: socket.socket, size):
+
+
+def short_read(socket: socket.socket, size):
     data = b""
     while len(data) < size:
         data2 = socket.recv(size - len(data))
@@ -12,8 +14,19 @@ def recv_all(socket: socket.socket, size):
         data += data2
     return data
 
+def short_write(socket: socket.socket, bytes):
+    total_sent = 0
+    while total_sent < len(bytes):
+        sent = socket.send(bytes[total_sent:])
+        if sent == 0:
+            raise RuntimeError("socket connection broken")
+        total_sent += sent
+    return total_sent
+
+def recv_all(socket: socket.socket, size):
+    return short_read(socket, size)
 
 def send_all(socket: socket.socket, bytes):
-    return socket.send(bytes)
+    return short_write(socket, bytes)
 
 
